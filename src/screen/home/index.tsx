88594fa9e501def2ba6816d29422018e30641d53
Gustavo <gustavo.2024014368@alunocesul.com>
2025-09-15 17:16:16 -0300

    funcition alterar

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -1,6 +1,7 @@
 import { Alert, FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { useState } from "react";
+import { Product } from "../../components/Product";
 
 export function Home() {
     
@@ -14,29 +15,48 @@ type ProductType = {
   const [finishedProducts, setFinishedProducts] = useState<ProductType[]>([]);
   const [produtoName, setProductName] = useState("");
 
-  function handleAddProduct() {
-    if (!produtoName.trim()) return;
+    function handleAddProduct() {
+        if (!produtoName.trim()) return;
 
-    if (
-      products.find((p) => p.nome === produtoName) ||
-      finishedProducts.find((p) => p.nome === produtoName)
-    ) {
-      return Alert.alert(
-        "Este cadastrado ja foi feito!",
-        "Na sua lista já exite um cadastrado com esse nome."
-      );
+        if (
+        products.find((p) => p.nome === produtoName) ||
+        finishedProducts.find((p) => p.nome === produtoName)
+        ) {
+        return Alert.alert(
+            "Este cadastrado ja foi feito!",
+            "Na sua lista já exite um cadastrado com esse nome."
+        );
+        }
+
+        const newProduct: ProductType = {
+        id: String(new Date().getTime()),
+        nome: produtoName,
+        concluido: false,
+        };
+
+        setProducts((prev) => [...prev, newProduct]);
+        setProductName("");
     }
 
-    const newProduct: ProductType = {
-      id: String(new Date().getTime()),
-      nome: produtoName,
-      concluido: false,
-    };
-
-    setProducts((prev) => [...prev, newProduct]);
-    setProductName("");
-  }
-
+    function handleProductToggle(id: string) {
+    
+        const product = products.find((p) => p.id === id);
+        if (product) {
+        const updated = { ...product, completed: true };
+        setProducts((prev) => prev.filter((p) => p.id !== id));
+        setFinishedProducts((prev) => [...prev, updated]);
+        return;
+        }
+        
+        const finished = finishedProducts.find((p) => p.id === id);
+        if (finished) {
+        const updated = { ...finished, completed: false };
+        setFinishedProducts((prev) => prev.filter((p) => p.id !== id));
+        setProducts((prev) => [...prev, updated]);
+        }
+    }
+    
+  const allProducts = [...products, ...finishedProducts];
 
     return (
     <View style={styles.container}>
@@ -59,27 +79,26 @@ type ProductType = {
         <View style={styles.contadores}>
           <View style={styles.contadoresBox}>
             <Text style={styles.produtos}>Produtos</Text>
-            <Text style={styles.contadorProdutos}></Text>
+            <Text style={styles.contadorProdutos}>{finishedProducts.length}</Text>
           </View>
           <View style={styles.contadoresBox}>
             <Text style={styles.finalizados}>Finalizados</Text>
-            <Text style={styles.contadorProdutos}></Text>
+            <Text style={styles.contadorProdutos}>{finishedProducts.length}</Text>
           </View>
         </View>
 
         <View style={styles.listaVazia}>
-            <Image
-            source={require("../../../assets/lista.png")}
-            style={styles.imagemLista}
-            />
-            <Text style={styles.txtListaVazia1}>
-            Você ainda não tem produtos na lista de compra
-            </Text>
-            <Text style={styles.txtListaVazia2}>
-            Adicione produtos e organize sua lista de compras
-            </Text>
-        </View>
-
+              <Image
+                source={require("../../../assets/lista.png")}
+                style={styles.imagemLista}
+              />
+              <Text style={styles.txtListaVazia1}>
+                Você ainda não tem produtos na lista de compra
+              </Text>
+              <Text style={styles.txtListaVazia2}>
+                Adicione produtos e organize sua lista de compras
+              </Text>
+            </View>
         </View>
     </View>
 
