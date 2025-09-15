585a264a43040cc5874e8c1fb532c03c792a07db
Gustavo <gustavo.2024014368@alunocesul.com>
2025-09-15 16:49:30 -0300

    input e botao

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
@@ -7,6 +7,21 @@ export function Home() {
       <View style={styles.top}>
         <Text style={styles.txtTop}> Lista de Compras </Text>
       </View>
+
+        <View style={styles.meio}>
+            <View style={styles.form}>
+            <TextInput
+                style={styles.input}
+                placeholder="Adicione um novo produto"
+                placeholderTextColor="#808080"
+            />
+            <TouchableOpacity style={styles.button}>
+                <Image source={require("../../../assets/plus.png")} />
+            </TouchableOpacity>
+            </View>
+        </View>
+
     </View>
+
     );
 }
--- src/screen/home/styles.tsx
+++ src/screen/home/styles.tsx
@@ -18,5 +18,35 @@ export const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: "700",
         lineHeight: 25.2,
+    },meio: {
+        paddingHorizontal: 24,
+        backgroundColor: '#f2f2f2',
+        alignItems: 'center',
+        paddingTop: 8,
+        paddingBottom: 8,
+    },
+
+    form: {
+        flexDirection: 'row',
+        marginTop: -35,
+        width: "100%",
+    },
+
+    input: {
+        flex: 1,
+        backgroundColor :'#f2f2f2',
+        borderRadius:6,
+        borderWidth: 0.5,
+        borderColor:'#808080',
+        padding: 16,
+        marginRight: 4
+    },
+        button: {
+        width: 52,
+        height: 52,
+        justifyContent: 'center',
+        alignItems:'center',
+        backgroundColor:'#31C667',
+        borderRadius:6,
     },
   });
